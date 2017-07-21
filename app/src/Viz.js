36cf5bb6a75629e279afadcee6516bf979e5da2d
Steve Hall <hall.steven.g@gmail.com>
2017-07-21 13:31:39 -0700

    use force 3d

--- app/src/Viz.js
+++ app/src/Viz.js
@@ -6,34 +6,35 @@ import {
   forceSimulation,
   forceManyBody,
   forceLink,
-  forceX,
-  forceY
-} from "d3-force";
-
-// export {default as forceCenter} from "./src/center";
-// export {default as forceCollide} from "./src/collide";
-// export {default as forceLink} from "./src/link";
-// export {default as forceManyBody} from "./src/manyBody";
-// export {default as forceSimulation} from "./src/simulation";
-// export {default as forceX} from "./src/x";
-// export {default as forceY} from "./src/y";
+  forceCenter
+} from "d3-force-3d";
 
 class Viz extends Component {
   componentDidMount() {
     const { canvas } = this;
     const { innerWidth, innerHeight } = window;
-    const aspect = innerWidth / innerHeight;
 
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera();
-    camera.position.z = 800;
+    camera.position.z = 500;
 
     const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
     renderer.setSize(innerWidth, innerHeight);
     renderer.setClearColor(0x333333);
 
-    const light = new THREE.AmbientLight(0xffffff);
-    scene.add(light);
+    const spotLight = new THREE.SpotLight(0xffffff);
+    spotLight.position.set(-500, 1000, 0);
+
+    spotLight.castShadow = true;
+
+    spotLight.shadow.mapSize.width = 1024;
+    spotLight.shadow.mapSize.height = 1024;
+
+    spotLight.shadow.camera.near = 500;
+    spotLight.shadow.camera.far = 4000;
+    spotLight.shadow.camera.fov = 30;
+
+    scene.add(spotLight);
 
     window.addEventListener("resize", onWindowResize, false);
 
@@ -56,16 +57,17 @@ class Viz extends Component {
       };
     });
 
-    const simulation = forceSimulation(nodes)
-      .force("charge", forceManyBody())
+    forceSimulation()
+      .numDimensions(3)
+      .nodes(nodes)
       .force("link", forceLink(links).distance(20).strength(1))
-      .force("x", forceX())
-      .force("y", forceY())
+      .force("charge", forceManyBody())
+      .force("center", forceCenter())
       .on("tick", ticked);
 
     const rootNode = select(scene);
     const container = rootNode.append("object");
-    const geometry = new THREE.SphereGeometry(5, 10, 10);
+    const geometry = new THREE.SphereGeometry(2, 10, 10);
     const material = new THREE.MeshPhongMaterial({
       color: "blue",
       shininess: 80
@@ -78,13 +80,13 @@ class Viz extends Component {
       .append("mesh")
       .attr("material", material)
       .attr("geometry", geometry)
-      .attr("position", d => {
+      .attr("position", () => {
         return { x: 0, y: 0, z: 0 };
       });
 
     function ticked() {
-      node.attr("position", d => {
-        return { x: d.x || 0, y: d.y || 0, z: 0 };
+      node.attr("position", ({ x, y, z }) => {
+        return { x, y, z };
       });
     }
 
