935e5f77f99c96da25e423393d612c75e125b4a6
Steve Hall <hall.steven.g@gmail.com>
2017-07-21 00:11:52 -0700

    add three scene

--- app/src/Viz.js
+++ app/src/Viz.js
@@ -1,8 +1,29 @@
 import React, { Component } from "react";
+import THREE from "three";
 
 class Viz extends Component {
   componentDidMount() {
-    console.log(this.canvas);
+    const { canvas } = this;
+    const { innerWidth, innerHeight } = window;
+
+    console.log(canvas);
+
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      innerWidth / innerHeight,
+      1,
+      6000
+    );
+    camera.position.z = 1000;
+
+    const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
+    renderer.setSize(innerWidth, innerHeight);
+    renderer.setClearColor(0x333333);
+
+    const light = new THREE.HemisphereLight("#fff", "#666", 1.5);
+    light.position.set(0, 1000, 0);
+    scene.add(light);
   }
 
   render() {
