ecd545c4ec732dbc3aaee714648ece8c63c23243
Steve Hall <hall.steven.g@gmail.com>
2017-07-20 21:52:22 -0700

    create file param

--- app/src/index.js
+++ app/src/index.js
@@ -4,5 +4,12 @@ import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
+const fileParam = new URLSearchParams(
+  (window.location.search || "").slice(1)
+).get("file");
+
+const toLoadPath = fileParam !== null ? `/${fileParam}` : "/demo.json";
+console.log(toLoadPath);
+
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
