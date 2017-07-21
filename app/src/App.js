2223f489e1abba16f18a929367b45e0f0b17bcc2
Steve Hall <hall.steven.g@gmail.com>
2017-07-20 21:53:30 -0700

    update index

--- app/src/App.js
+++ app/src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <div className="App" >
+      <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
