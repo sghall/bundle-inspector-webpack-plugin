efcb434013ef24c15af90a55f0544aa6d3b1fc36
Steve Hall <hall.steven.g@gmail.com>
2017-07-22 13:55:58 -0700

    clean up

--- app/src/Treemap/Treemap.js
+++ app/src/Treemap/Treemap.js
@@ -1,31 +1,20 @@
 import React, { Component } from "react";
 import { scaleLinear } from "d3-scale";
-import { interpolateCubehelixLong } from "d3-interpolate";
-import * as THREE from "three";
-import OrbitControls from "../OrbitControls";
-import { select } from "subunit";
-import {
-  forceSimulation,
-  forceManyBody,
-  forceLink,
-  forceCenter
-} from "d3-force-3d";
-import colors from "./colors";
-import raycast from "./raycast";
-
-const cubhelix = interpolateCubehelixLong("blue", "red");
 
 class Graph extends Component {
   componentDidMount() {
-    var margin = { top: 20, right: 0, bottom: 0, left: 0 },
-      width = 960,
-      height = 500 - margin.top - margin.bottom,
-      formatNumber = d3.format(",d"),
-      transitioning;
+    const { data } = this.props;
 
-    var x = d3.scale.linear().domain([0, width]).range([0, width]);
+    const margin = { top: 20, right: 0, bottom: 0, left: 0 };
+    const width = 960;
+    const height = 500 - margin.top - margin.bottom;
+    const formatNumber = d3.format(",d");
 
-    var y = d3.scale.linear().domain([0, height]).range([0, height]);
+    let transitioning;
+
+    var x = scaleLinear().domain([0, width]).range([0, width]);
+
+    var y = scaleLinear().domain([0, height]).range([0, height]);
 
     var treemap = d3.layout
       .treemap()
@@ -63,158 +52,147 @@ class Graph extends Component {
       .attr("y", 6 - margin.top)
       .attr("dy", ".75em");
 
-    d3.json("flare.json", function(root) {
-      initialize(root);
-      accumulate(root);
-      layout(root);
-      display(root);
+    initialize(data);
+    accumulate(data);
+    layout(data);
+    display(data);
 
-      function initialize(root) {
-        root.x = root.y = 0;
-        root.dx = width;
-        root.dy = height;
-        root.depth = 0;
+    function initialize(root) {
+      root.x = root.y = 0;
+      root.dx = width;
+      root.dy = height;
+      root.depth = 0;
+    }
+
+    function accumulate(d) {
+      return (d._children = d.children)
+        ? (d.value = d.children.reduce(function(p, v) {
+            return p + accumulate(v);
+          }, 0))
+        : d.value;
+    }
+
+    function layout(d) {
+      if (d._children) {
+        treemap.nodes({ _children: d._children });
+        d._children.forEach(function(c) {
+          c.x = d.x + c.x * d.dx;
+          c.y = d.y + c.y * d.dy;
+          c.dx *= d.dx;
+          c.dy *= d.dy;
+          c.parent = d;
+          layout(c);
+        });
+      }
+    }
+
+    function display(d) {
+      grandparent
+        .datum(d.parent)
+        .on("click", transition)
+        .select("text")
+        .text(name(d));
+
+      var g1 = svg.insert("g", ".grandparent").datum(d).attr("class", "depth");
+
+      var g = g1.selectAll("g").data(d._children).enter().append("g");
+
+      g
+        .filter(function(d) {
+          return d._children;
+        })
+        .classed("children", true)
+        .on("click", transition);
+
+      g
+        .selectAll(".child")
+        .data(function(d) {
+          return d._children || [d];
+        })
+        .enter()
+        .append("rect")
+        .attr("class", "child")
+        .call(rect);
+
+      g
+        .append("rect")
+        .attr("class", "parent")
+        .call(rect)
+        .append("title")
+        .text(function(d) {
+          return formatNumber(d.value);
+        });
+
+      g
+        .append("text")
+        .attr("dy", ".75em")
+        .text(function(d) {
+          return d.name;
+        })
+        .call(text);
+
+      function transition(d) {
+        if (transitioning || !d) return;
+        transitioning = true;
+
+        var g2 = display(d),
+          t1 = g1.transition().duration(750),
+          t2 = g2.transition().duration(750);
+
+        x.domain([d.x, d.x + d.dx]);
+        y.domain([d.y, d.y + d.dy]);
+
+        svg.style("shape-rendering", null);
+
+        svg.selectAll(".depth").sort(function(a, b) {
+          return a.depth - b.depth;
+        });
+
+        g2.selectAll("text").style("fill-opacity", 0);
+
+        t1.selectAll("text").call(text).style("fill-opacity", 0);
+        t2.selectAll("text").call(text).style("fill-opacity", 1);
+        t1.selectAll("rect").call(rect);
+        t2.selectAll("rect").call(rect);
+
+        t1.remove().each("end", function() {
+          svg.style("shape-rendering", "crispEdges");
+          transitioning = false;
+        });
       }
 
-      function accumulate(d) {
-        return (d._children = d.children)
-          ? (d.value = d.children.reduce(function(p, v) {
-              return p + accumulate(v);
-            }, 0))
-          : d.value;
-      }
+      return g;
+    }
 
-      function layout(d) {
-        if (d._children) {
-          treemap.nodes({ _children: d._children });
-          d._children.forEach(function(c) {
-            c.x = d.x + c.x * d.dx;
-            c.y = d.y + c.y * d.dy;
-            c.dx *= d.dx;
-            c.dy *= d.dy;
-            c.parent = d;
-            layout(c);
-          });
-        }
-      }
+    function text(text) {
+      text
+        .attr("x", function(d) {
+          return x(d.x) + 6;
+        })
+        .attr("y", function(d) {
+          return y(d.y) + 6;
+        });
+    }
 
-      function display(d) {
-        grandparent
-          .datum(d.parent)
-          .on("click", transition)
-          .select("text")
-          .text(name(d));
+    function rect(rect) {
+      rect
+        .attr("x", function(d) {
+          return x(d.x);
+        })
+        .attr("y", function(d) {
+          return y(d.y);
+        })
+        .attr("width", function(d) {
+          return x(d.x + d.dx) - x(d.x);
+        })
+        .attr("height", function(d) {
+          return y(d.y + d.dy) - y(d.y);
+        });
+    }
 
-        var g1 = svg
-          .insert("g", ".grandparent")
-          .datum(d)
-          .attr("class", "depth");
-
-        var g = g1.selectAll("g").data(d._children).enter().append("g");
-
-        g
-          .filter(function(d) {
-            return d._children;
-          })
-          .classed("children", true)
-          .on("click", transition);
-
-        g
-          .selectAll(".child")
-          .data(function(d) {
-            return d._children || [d];
-          })
-          .enter()
-          .append("rect")
-          .attr("class", "child")
-          .call(rect);
-
-        g
-          .append("rect")
-          .attr("class", "parent")
-          .call(rect)
-          .append("title")
-          .text(function(d) {
-            return formatNumber(d.value);
-          });
-
-        g
-          .append("text")
-          .attr("dy", ".75em")
-          .text(function(d) {
-            return d.name;
-          })
-          .call(text);
-
-        function transition(d) {
-          if (transitioning || !d) return;
-          transitioning = true;
-
-          var g2 = display(d),
-            t1 = g1.transition().duration(750),
-            t2 = g2.transition().duration(750);
-
-          // Update the domain only after entering new elements.
-          x.domain([d.x, d.x + d.dx]);
-          y.domain([d.y, d.y + d.dy]);
-
-          // Enable anti-aliasing during the transition.
-          svg.style("shape-rendering", null);
-
-          // Draw child nodes on top of parent nodes.
-          svg.selectAll(".depth").sort(function(a, b) {
-            return a.depth - b.depth;
-          });
-
-          // Fade-in entering text.
-          g2.selectAll("text").style("fill-opacity", 0);
-
-          // Transition to the new view.
-          t1.selectAll("text").call(text).style("fill-opacity", 0);
-          t2.selectAll("text").call(text).style("fill-opacity", 1);
-          t1.selectAll("rect").call(rect);
-          t2.selectAll("rect").call(rect);
-
-          // Remove the old node when the transition is finished.
-          t1.remove().each("end", function() {
-            svg.style("shape-rendering", "crispEdges");
-            transitioning = false;
-          });
-        }
-
-        return g;
-      }
-
-      function text(text) {
-        text
-          .attr("x", function(d) {
-            return x(d.x) + 6;
-          })
-          .attr("y", function(d) {
-            return y(d.y) + 6;
-          });
-      }
-
-      function rect(rect) {
-        rect
-          .attr("x", function(d) {
-            return x(d.x);
-          })
-          .attr("y", function(d) {
-            return y(d.y);
-          })
-          .attr("width", function(d) {
-            return x(d.x + d.dx) - x(d.x);
-          })
-          .attr("height", function(d) {
-            return y(d.y + d.dy) - y(d.y);
-          });
-      }
-
-      function name(d) {
-        return d.parent ? name(d.parent) + "." + d.name : d.name;
-      }
-    });
+    function name(d) {
+      return d.parent ? name(d.parent) + "." + d.name : d.name;
+    }
   }
 
   render() {
