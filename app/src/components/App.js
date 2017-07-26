bba9d0c366297f2cd8812ff38e5bdbceaa95b81b
Steve Hall <hall.steven.g@gmail.com>
2017-07-26 00:22:48 -0700

    use HashRouter

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import { createColors } from "./utils";
 import TopNav from "./TopNav";
 import GraphPage from "./GraphPage";
