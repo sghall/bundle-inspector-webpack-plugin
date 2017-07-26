7e9c2f3be0653e3cea306c4c3ff8f40ab21aa10e
Steve Hall <hall.steven.g@gmail.com>
2017-07-25 23:42:30 -0700

    update gh-pages

--- src/process.js
+++
@@ -1,8 +0,0 @@
-const { getViewerData } = require("webpack-bundle-analyzer/lib/analyzer");
-
-module.exports = {
-  processStats: stats => {
-    const processed = getViewerData(stats);
-    return JSON.stringify(processed);
-  }
-};
--- src/utils.js
+++
@@ -1,7 +0,0 @@
-const path = require("path");
-
-module.exports = {
-  getWritePathForSerializedData: (dataPath, contextPath = __dirname) => {
-    return path.join(contextPath, "/../app/build", dataPath);
-  }
-};
