3fc1dc8f068c20243e6917a8aaab090956b815de
Steve Hall <hall.steven.g@gmail.com>
2017-07-25 21:23:49 -0700

    send back update function on server launch

--- src/server.js
+++ src/server.js
@@ -7,35 +7,40 @@ const ws = require("ws");
 const { yellow } = require("chalk");
 
 module.exports = function launchServer(dataFile) {
+  const app = express();
+
+  app.use(express.static(path.join(__dirname, "/../app/build")));
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/../app/build", "index.html"));
+  });
+
+  const server = http.createServer(app);
+
   openPort.find((err, port) => {
     if (err != null) {
       console.log(err);
       process.exit(1);
     }
 
-    const app = express();
-
-    app.use(express.static(path.join(__dirname, "/../app/build")));
-    app.get("/", (req, res) => {
-      res.sendFile(path.join(__dirname, "/../app/build", "index.html"));
-    });
-
-    const server = http.createServer(app);
-
     server.listen(port, () => {
       console.log(yellow(`Chunky Monkey listening on port ${port}`));
       console.log(yellow(`Press Control+C to Quit`));
       opn(`http://localhost:${port}?file=${dataFile}`);
     });
-
-    const wss = new ws.Server({ server });
-
-    return function(dataFile) {
-      wss.clients.forEach(c => {
-        if (c.readyState === ws.OPEN) {
-          c.send(dataFile);
-        }
-      });
-    };
   });
+
+  const wss = new ws.Server({ server });
+
+  function update(d) {
+    wss.clients.forEach(c => {
+      console.log(yellow(`Chunky Monkey: ${d}`));
+      if (c.readyState === ws.OPEN) {
+        c.send(d);
+      }
+    });
+
+    return update;
+  }
+
+  return update;
 };
